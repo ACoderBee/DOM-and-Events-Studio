8a444dd899e4e81ce843ff2aba93428a88624433
Andrea I <121627031+ACoderBee@users.noreply.github.com>
2023-04-10 13:22:47 -0500

    Initial changes

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener(load, function(){
+
+let takeoffBtn = document.getElementById("takeoff");//event
+takeoffBtn.addEventListener("click", function(){
+    window.confirm("Confirm that the shuttle is ready for takeoff.");
+});
+
+
+
+
+
+});
