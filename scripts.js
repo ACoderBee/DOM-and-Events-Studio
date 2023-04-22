d0056f96a141c99b30fe0d7b244396c5e6cbe825
Andrea <93172228+Arionly@users.noreply.github.com>
2023-04-22 16:23:45 -0500

    changes to abort button and add rocket to css

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 window.addEventListener('load', function(){
-    const takeoffBtn = document.getElementById("takeoff");//event
-    const landBtn = document.getElementById("landing"); //event
+    const takeoffBtn = document.getElementById("takeoff");//event obj
+    const landBtn = document.getElementById("landing"); //event obj
+    const abortBtn = document.getElementById("missionAbort");//event obj
+    
     const flightStatus = document.getElementById("flightStatus");//p element
     const shuttleBackground = document.getElementById("shuttleBackground");//div element
     const spaceShuttleHeight = document.getElementById('spaceShuttleHeight');//p element
@@ -20,11 +22,16 @@ window.addEventListener('load', function(){
     landBtn.addEventListener('click',function () {
 //         A window alert should let the user know "The shuttle is landing. Landing gear engaged."
         let isReadyForLanding = window.alert("The shuttle is landing. Landing gear engaged.");
-        console.log(alert);
         flightStatus.innerText = "The shuttle has landed.";
-        shuttleBackground.setAttribute("style", "backgroundColor=green");//setAttribute not preferred
+        shuttleBackground.setAttribute("style", "backgroundColor = green");//setAttribute not preferred
         shuttleAltitude = 0;
         spaceShuttleHeight.innerText = shuttleAltitude;
     });
-    
+    abortBtn.addEventListener('click', function(){
+        let isAbortingMission = window.confirm( "Confirm that you want to abort the mission.")
+        flightStatus.innerText = "Mission aborted.";
+        shuttleBackground.style.backgroundColor = "green";//using preferred style
+        shuttleAltitude = 0;
+        spaceShuttleHeight.innerText = shuttleAltitude;
+    })
 });
